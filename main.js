114e64532f02abef4ed0c04d2a00313660b97136
Mpanasetckiy <maxbuzz8694@gmail.com>
2023-05-02 22:33:57 +0100

    style(main.js): fix indentation and add semicolons style(project.css): fix indentation and add missing newline at end of file

--- main.js
+++ main.js
@@ -1,44 +1,44 @@
 // Get DOM elements
-const links = document.querySelector('.footer__links');
+const links = document.querySelector(".footer__links");
 
 // Assign global variables
 const flipTimeouts = new Map();
 
 // Functionality
 const bounceLink = (link) => {
-    if (!flipTimeouts.has(link)) {
-        const timeoutId = setTimeout(function() {
-            if (hover && !link.classList.contains('fa-flip')) {
-                link.classList.add('fa-flip')
-            }
-            flipTimeouts.delete(link);
-        }, 300);
-        flipTimeouts.set(link, timeoutId);
-    }
-}
+  if (!flipTimeouts.has(link)) {
+    const timeoutId = setTimeout(function () {
+      if (hover && !link.classList.contains("fa-flip")) {
+        link.classList.add("fa-flip");
+      }
+      flipTimeouts.delete(link);
+    }, 300);
+    flipTimeouts.set(link, timeoutId);
+  }
+};
 
 const removeBounce = (link) => {
-    const timeoutId = flipTimeouts.get(link);
-    if (timeoutId) {
-        clearTimeout(timeoutId);
-        flipTimeouts.delete(link);
-    }
-    if (!hover && link.classList.contains('fa-flip')) {
-        link.classList.remove('fa-flip');
-    }
-}
+  const timeoutId = flipTimeouts.get(link);
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+    flipTimeouts.delete(link);
+  }
+  if (!hover && link.classList.contains("fa-flip")) {
+    link.classList.remove("fa-flip");
+  }
+};
 
 // List of event listeners
-links.addEventListener('mouseover', (event) => {
-    if (event.target.tagName === 'I') {
-        hover = true;
-        bounceLink(event.target);
-    }
+links.addEventListener("mouseover", (event) => {
+  if (event.target.tagName === "I") {
+    hover = true;
+    bounceLink(event.target);
+  }
 });
 
-links.addEventListener('mouseout', (event) => {
-    if (event.target.tagName === 'I') {
-        hover = false;
-        removeBounce(event.target);
-    }
-})
+links.addEventListener("mouseout", ({ target }) => {
+  if (target.tagName === "I") {
+    hover = false;
+    removeBounce(event.target);
+  }
+});
